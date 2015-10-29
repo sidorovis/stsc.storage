ac829304805936a9e32713429a2f1ed8fd9fe786
Ivan Sidarau <isidarau@360t.com>
2015-10-29 11:46:54 +0100

    deleted AlgorithmSetting and implementation

--- src/main/java/stsc/storage/ExecutionsStorage.java
+++ src/main/java/stsc/storage/ExecutionsStorage.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import stsc.algorithms.AlgorithmConfigurationImpl;
 import stsc.algorithms.EodOutput;
 import stsc.algorithms.Output;
 import stsc.common.algorithms.AlgorithmNameGenerator;
 import stsc.common.algorithms.BadAlgorithmException;
 import stsc.common.algorithms.EodExecution;
+import stsc.common.algorithms.MutatingAlgorithmConfiguration;
 import stsc.common.algorithms.StockExecution;
 import stsc.common.trading.Broker;
 
@@ -110,7 +110,7 @@ public final class ExecutionsStorage implements Cloneable {
 		final ArrayList<String> names = new ArrayList<>();
 		final ArrayList<StockExecution> initialList = new ArrayList<>(getStockExecutions());
 		for (StockExecution stockExecution : initialList) {
-			final AlgorithmConfigurationImpl as = new AlgorithmConfigurationImpl();
+			final MutatingAlgorithmConfiguration as = stockExecution.getSettings().createAlgorithmConfiguration();
 			final String executionName = stockExecution.getExecutionName();
 			as.addSubExecutionName(executionName);
 			names.add(executionName);
@@ -129,7 +129,7 @@ public final class ExecutionsStorage implements Cloneable {
 		final ArrayList<String> names = new ArrayList<>();
 		final ArrayList<EodExecution> initialList = new ArrayList<>(getEodExecutions());
 		for (EodExecution eodExecution : initialList) {
-			final AlgorithmConfigurationImpl as = new AlgorithmConfigurationImpl();
+			final MutatingAlgorithmConfiguration as = eodExecution.getSettings().createAlgorithmConfiguration();
 			final String executionName = eodExecution.getExecutionName();
 			as.addSubExecutionName(executionName);
 			names.add(executionName);
